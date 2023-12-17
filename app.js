c021279936ae43aeca7cce1ea9cfd9ee7f09386d
Neno Arisma <nenoarisma@gmail.com>
2023-12-17 20:11:49 +0700

    feat: adding multer this package are for handling multipart/form-data
    i know its bloated but somehow we are very lazy to use json to send the data
    lmao

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ import Express from "express";
 import bodyParser from "body-parser";
 import { Server } from "socket.io";
 import cors from "cors";
+import multer from "multer";
 
 // Import own code
 import authRoute from "./src/routes/auth.route.js";
 
 // starterrrrrpacckkkkkkkkkkkk
 const app = Express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
+const formData = multer();
 const io = new Server(3001, {});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
+// using multipart/form-data
+app.use(formData.array());
 
 app.use("/api/v1/auth", authRoute);
 app.get("/", (req, res) => {
