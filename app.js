12ec21c5ea0e3021d6c2de3fd0cd4b281d40ef35
Neno Arisma <nenoarisma@gmail.com>
2023-12-17 17:44:07 +0700

    Add dependencies and enable CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import Express from "express";
 import bodyParser from "body-parser";
 import { Server } from "socket.io";
+import cors from "cors";
 
 // Import own code
 import authRoute from "./src/routes/auth.route.js";
@@ -12,6 +13,7 @@ const port = process.env.PORT || 8080;
 const io = new Server(3001, {});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
 
 app.use("/api/v1/auth", authRoute);
 app.get("/", (req, res) => {
